e9cb04b25c69994d3ee2e5d66625cc9461976d61
nik <nikdelig@gmail.com>
2017-04-15 00:47:44 +0300

    Some more adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 const config = {
   devtool: 'source-map',
-  entry: ['webpack/hot/dev-server' , './src/js/index.js'],
+  entry: [
+    'babel-polyfill',
+    'webpack/hot/dev-server',
+    './src/js/index.js',
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
@@ -18,7 +23,8 @@ const config = {
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({template: './index.html'})
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new ProgressBarPlugin(),
   ],
 };
 
