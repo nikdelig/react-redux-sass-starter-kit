d905fcd6e32b4d95d7111360186ebb592d0e979a
nik <nikdelig@gmail.com>
2017-04-15 13:30:00 +0300

    Some more adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var webpack = require('gulp-webpack');
 var minifyCSS = require('gulp-csso');
 var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean');
 
 gulp.task('sass', function () {
     return gulp.src('./src/sass/**/*.sass')
@@ -10,10 +11,15 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['clean'], function() {
     return gulp.src('src/js/index.js')
       .pipe(webpack( require('./webpack.config.js') ))
       .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', [ 'sass' ]);
+gulp.task('clean', function() {
+    return gulp.src('./dist')
+    .pipe(clean());
+});
+
+gulp.task('default', ['sass', 'images', 'clean']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
 var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
-const config = {
-  devtool: 'source-map',
-  entry: [
-    'babel-polyfill',
-    'webpack/hot/dev-server',
-    './src/js/index.js',
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
-  module: {
-      rules: [{
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-      }]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({template: './index.html'}),
-    new ProgressBarPlugin(),
-  ],
+var config = {
+    devtool: 'source-map',
+    entry: [
+        'babel-polyfill',
+        'webpack/hot/dev-server',
+        './src/js/index.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+    ],
 };
 
 module.exports = config;
